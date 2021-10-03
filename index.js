70a54093f971dc819111a7e6609c0a6f75413a68
alezys <alezys@mail.ru>
2021-10-03 11:40:56 +0300

    next change homework_16

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //-------------------------------------------Task#1
 let pulp = document.querySelector(`#pulp`);
 for (let i = 0; i < pulp.children.length; i++) {
-    for (let j = 0; j < pulp.children[i].children.length; j++) {
-        let str = pulp.children[i].children[j].textContent;
-        if (str.match(/http/gi) !== null) {
-            pulp.children[i].children[j].style.borderBottom = `dotted`;
-        }
-    }
+	for (let j = 0; j < pulp.children[i].children.length; j++) {
+		let str = pulp.children[i].children[j].textContent;
+		if (str.match(/http/gi) !== null) {
+			pulp.children[i].children[j].style.borderBottom = `dotted`;
+		}
+	}
 }
 
 //------------------------------------------Task#2
@@ -14,35 +14,35 @@ for (let i = 0; i < pulp.children.length; i++) {
 let listDisk = document.querySelector(`#listDisk`);
 let spanList = listDisk.getElementsByTagName(`span`);
 for (let i = 0; i < spanList.length; i++) {
-    spanList[i].id = `spanID` + [i];
+	spanList[i].id = `spanID` + [i];
 }
 function textFat(event) {
-    for (let i = 0; i < spanList.length; i++) {
-        if (spanList[i].id === event.target.id) {
-            spanList[i].style.fontWeight = `bold`;
-        }
-    }
+	for (let i = 0; i < spanList.length; i++) {
+		if (spanList[i].id === event.target.id) {
+			spanList[i].style.fontWeight = `bold`;
+		}
+	}
 }
 function textNormal(event) {
-    for (let i = 0; i < spanList.length; i++) {
-        if (spanList[i].id === event.target.id) {
-            spanList[i].style.fontWeight = `normal`;
-        }
-    }
+	for (let i = 0; i < spanList.length; i++) {
+		if (spanList[i].id === event.target.id) {
+			spanList[i].style.fontWeight = `normal`;
+		}
+	}
 }
 function textHidden(event) {
-    for (let i = 0; i < spanList.length; i++) {
-        if (spanList[i].id === event.target.id) {
-            let childrenSpan = spanList[i].parentNode.getElementsByTagName(`ul`)
-            for (let j = 0; j < childrenSpan.length; j++) {
-                if (childrenSpan[j].hidden === false) {
-                    childrenSpan[j].hidden = true;
-                } else {
-                    childrenSpan[j].hidden = false;
-                }
-            }
-        }
-    }
+	for (let i = 0; i < spanList.length; i++) {
+		if (spanList[i].id === event.target.id) {
+			let childrenSpan = spanList[i].parentNode.getElementsByTagName(`ul`)
+				for (let j = 0; j < childrenSpan.length; j++) {
+					if (childrenSpan[j].hidden === false) {
+						childrenSpan[j].hidden = true;
+					} else {
+						childrenSpan[j].hidden = false;
+					}
+				}
+		}
+	}
 }
 listDisk.addEventListener(`mouseover`, textFat);
 listDisk.addEventListener(`mouseout`, textNormal);
@@ -54,45 +54,45 @@ let bibl = document.querySelector(`#bibl`);
 let start = ``;
 let spanBooks = bibl.getElementsByTagName(`span`);
 for (let i = 0; i < spanBooks.length; i++) {
-    spanBooks[i].id = `bookID` + [i];
+	spanBooks[i].id = `bookID` + [i];
 }
 function textPick(event) {
-    if (event.ctrlKey !== true && event.shiftKey !== true) {
-        for (let i = 0; i < spanBooks.length; i++) {
-            if (spanBooks[i].id === event.target.id && spanBooks[i].style.backgroundColor !== `orange`) {
-                spanBooks[i].style.backgroundColor = `orange`;
-                start = i;
-            } else if (spanBooks[i].style.backgroundColor === `orange`) {
-                spanBooks[i].style.backgroundColor = `inherit`;
-            }
-        }
-    }
-    if (event.ctrlKey === true && event.shiftKey !== true) {
-        for (let i = 0; i < spanBooks.length; i++) {
-            if (spanBooks[i].id === event.target.id && spanBooks[i].style.backgroundColor !== `orange` && event.ctrlKey === true) {
-                spanBooks[i].style.backgroundColor = `orange`;
-                start = i;
-            } else if (spanBooks[i].id === event.target.id && spanBooks[i].style.backgroundColor === `orange` && event.ctrlKey === true) {
-                spanBooks[i].style.backgroundColor = `inherit`;
-            }
-        }
-    }
-    if (event.shiftKey === true) {
-        for (let i = 0; i < spanBooks.length; i++) {
-            if (spanBooks[i].id === event.target.id) {
-                if (start > i) {
-                    for (let j = i; j < start; j++) {
-                        spanBooks[j].style.backgroundColor = `orange`;
-                    }
-                }
-                if (start < i) {
-                    for (let j = i; j > start; j--) {
-                        spanBooks[j].style.backgroundColor = `orange`;
-                    }
-                }
-            }
-        }
-    }
+	if (event.ctrlKey !== true && event.shiftKey !== true) {
+		for (let i = 0; i < spanBooks.length; i++) {
+			if (spanBooks[i].id === event.target.id && spanBooks[i].style.backgroundColor !== `orange`) {
+				spanBooks[i].style.backgroundColor = `orange`;
+				start = i;
+			} else if (spanBooks[i].style.backgroundColor === `orange`) {
+				spanBooks[i].style.backgroundColor = `inherit`;
+			}
+		}
+	}
+	if (event.ctrlKey === true && event.shiftKey !== true) {
+		for (let i = 0; i < spanBooks.length; i++) {
+			if (spanBooks[i].id === event.target.id && spanBooks[i].style.backgroundColor !== `orange` && event.ctrlKey === true) {
+				spanBooks[i].style.backgroundColor = `orange`;
+				start = i;
+			} else if (spanBooks[i].id === event.target.id && spanBooks[i].style.backgroundColor === `orange` && event.ctrlKey === true) {
+				spanBooks[i].style.backgroundColor = `inherit`;
+			}
+		}
+	}
+	if (event.shiftKey === true) {
+		for (let i = 0; i < spanBooks.length; i++) {
+			if (spanBooks[i].id === event.target.id) {
+				if (start > i) {
+					for (let j = i; j < start; j++) {
+						spanBooks[j].style.backgroundColor = `orange`;
+					}
+				}
+				if (start < i) {
+					for (let j = i; j > start; j--) {
+					spanBooks[j].style.backgroundColor = `orange`;
+					}
+				}
+			}
+		}
+	}
 }
 bibl.addEventListener(`click`, textPick);
 
@@ -103,18 +103,18 @@ let contentText = document.querySelector(`#contentText`);
 let areaText = document.querySelector(`#areaText`);
 let str = ``;
 function textSelfHidden(event) {
-    if (event.code === `KeyE` && event.ctrlKey === true) {
-        event.preventDefault();
-    areaText.hidden = false;
-    contentText.hidden = true;
+	if (event.code === `KeyE` && event.ctrlKey === true) {
+		event.preventDefault();
+		areaText.hidden = false;
+		contentText.hidden = true;
 }
-    if (event.code === `KeyS` && event.ctrlKey === true) {
-        event.preventDefault();
-        areaText.hidden = true;
-        contentText.hidden = false;
-        str = areaText.value; 
-        contentText.innerText = str;
-    }
+	if (event.code === `KeyS` && event.ctrlKey === true) {
+		event.preventDefault();
+		areaText.hidden = true;
+		contentText.hidden = false;
+		str = areaText.value; 
+		contentText.innerText = str;
+	}
 }
 document.addEventListener(`keydown`, textSelfHidden);
 
@@ -125,75 +125,75 @@ let row = listName.getElementsByTagName(`tr`);
 let column = listName.getElementsByTagName(`th`);
 
 for (let i = 0; i < row.length; i++) {
-    row[i].id = `rowID` + [i];
+	row[i].id = `rowID` + [i];
 }
 for (let i = 0; i < column.length; i++) {
-    column[i].id = `columnID` + [i];
+	column[i].id = `columnID` + [i];
 }
 let unit1 = {
-    name: `Mark`,
-    lastname: `Zuckerberg`,
-    age: `34`,
-    company: `Facebook`
+	name: `Mark`,
+	lastname: `Zuckerberg`,
+	age: `34`,
+	company: `Facebook`
 }
 let unit2 = {
-    name: `Larry`,
-    lastname: `Page`,
-    age: `45`,
-    company: `Google`
+	name: `Larry`,
+	lastname: `Page`,
+	age: `45`,
+	company: `Google`
 }
 let unit3 = {
-    name: `Timothy`,
-    lastname: `Cook`,
-    age: `57`,
-    company: `Apple`
+	name: `Timothy`,
+	lastname: `Cook`,
+	age: `57`,
+	company: `Apple`
 }
 let unit4 = {
-    name: `Bill`,
-    lastname: `Gates`,
-    age: `62`,
-    company: `Microsoft`
+	name: `Bill`,
+	lastname: `Gates`,
+	age: `62`,
+	company: `Microsoft`
 }
 let arrPersons = [unit1, unit2, unit3, unit4, ];
 function sortNumberMax(a, b) {
-    if (a.age > b.age) return 1;
-    if (a.age === b.age) return 0;
-    if (a.age < b.age) return -1;
+	if (a.age > b.age) return 1;
+	if (a.age === b.age) return 0;
+	if (a.age < b.age) return -1;
 }
 function sortNumberMin(a, b) {
-    if (a.age < b.age) return 1;
-    if (a.age === b.age) return 0;
-    if (a.age > b.age) return -1;
+	if (a.age < b.age) return 1;
+	if (a.age === b.age) return 0;
+	if (a.age > b.age) return -1;
 }
 function sortStrNameMax(a, b) {
-    if (a.name > b.name) return 1;
-    if (a.name === b.name) return 0;
-    if (a.name < b.name) return -1;
+	if (a.name > b.name) return 1;
+	if (a.name === b.name) return 0;
+	if (a.name < b.name) return -1;
 }
 function sortStrNameMin(a, b) {
-    if (a.name < b.name) return 1;
-    if (a.name === b.name) return 0;
-    if (a.name > b.name) return -1;
+	if (a.name < b.name) return 1;
+	if (a.name === b.name) return 0;
+	if (a.name > b.name) return -1;
 }
 function sortStrLastNameMax(a, b) {
-    if (a.lastname > b.lastname) return 1;
-    if (a.lastname === b.lastname) return 0;
-    if (a.lastname < b.lastname) return -1;
+	if (a.lastname > b.lastname) return 1;
+	if (a.lastname === b.lastname) return 0;
+	if (a.lastname < b.lastname) return -1;
 }
 function sortStrLastNameMin(a, b) {
-    if (a.lastname < b.lastname) return 1;
-    if (a.lastname === b.lastname) return 0;
-    if (a.lastname > b.lastname) return -1;
+	if (a.lastname < b.lastname) return 1;
+	if (a.lastname === b.lastname) return 0;
+	if (a.lastname > b.lastname) return -1;
 }
 function sortStrCompanyMax(a, b) {
-    if (a.company > b.company) return 1;
-    if (a.company === b.company) return 0;
-    if (a.company < b.company) return -1;
+	if (a.company > b.company) return 1;
+	if (a.company === b.company) return 0;
+	if (a.company < b.company) return -1;
 }
 function sortStrCompanyMin(a, b) {
-    if (a.company < b.company) return 1;
-    if (a.company=== b.company) return 0;
-    if (a.company > b.company) return -1;
+	if (a.company < b.company) return 1;
+	if (a.company=== b.company) return 0;
+	if (a.company > b.company) return -1;
 }
 
 let checkerFirstName = 0;
@@ -202,59 +202,59 @@ let checkerAge = 0;
 let checkerCompany = 0;
 
 function optomizedCode() {
-    let countName = 4;
-    let countLastName = 5;
-    let countAge = 6;
-    let countCompany = 7;
-    for (let i = 0; i < arrPersons.length; i++) {
-        document.querySelector(`#columnID${countName}`).innerText = arrPersons[i].name;
-        document.querySelector(`#columnID${countLastName}`).innerText = arrPersons[i].lastname;
-        document.querySelector(`#columnID${countAge}`).innerText = arrPersons[i].age;
-        document.querySelector(`#columnID${countCompany}`).innerText = arrPersons[i].company;
-        countName = countName + 4;
-        countLastName = countLastName + 4;
-        countAge = countAge + 4;
-        countCompany = countCompany + 4;
+	let countName = 4;
+	let countLastName = 5;
+	let countAge = 6;
+	let countCompany = 7;
+	for (let i = 0; i < arrPersons.length; i++) {
+		document.querySelector(`#columnID${countName}`).innerText = arrPersons[i].name;
+		document.querySelector(`#columnID${countLastName}`).innerText = arrPersons[i].lastname;
+		document.querySelector(`#columnID${countAge}`).innerText = arrPersons[i].age;
+		document.querySelector(`#columnID${countCompany}`).innerText = arrPersons[i].company;
+		countName = countName + 4;
+		countLastName = countLastName + 4;
+		countAge = countAge + 4;
+		countCompany = countCompany + 4;
     }
 }
 
 function sortUnit(event) {
-    if (event.target.id === `columnID0` && checkerFirstName === 0) {
-        arrPersons.sort(sortStrNameMax);
-        optomizedCode();
-        checkerFirstName = 1;
-    } else if (event.target.id === `columnID0` && checkerFirstName === 1) {
-        arrPersons.sort(sortStrNameMin);
-        optomizedCode();
-        checkerFirstName = 0;
-    }
-    if (event.target.id === `columnID1` && checkerLastName === 0) {
-        arrPersons.sort(sortStrLastNameMax);
-        optomizedCode();
-        checkerLastName = 1;
-    } else if (event.target.id === `columnID1` && checkerLastName === 1) {
-        arrPersons.sort(sortStrLastNameMin);
-        optomizedCode();
-        checkerLastName = 0;
-    }
+	if (event.target.id === `columnID0` && checkerFirstName === 0) {
+		arrPersons.sort(sortStrNameMax);
+		optomizedCode();
+		checkerFirstName = 1;
+	} else if (event.target.id === `columnID0` && checkerFirstName === 1) {
+		arrPersons.sort(sortStrNameMin);
+		optomizedCode();
+		checkerFirstName = 0;
+	}
+	if (event.target.id === `columnID1` && checkerLastName === 0) {
+		arrPersons.sort(sortStrLastNameMax);
+		optomizedCode();
+		checkerLastName = 1;
+	} else if (event.target.id === `columnID1` && checkerLastName === 1) {
+		arrPersons.sort(sortStrLastNameMin);
+		optomizedCode();
+		checkerLastName = 0;
+	}
     if (event.target.id === `columnID2` && checkerAge === 0) {
-        arrPersons.sort(sortNumberMax);
-        optomizedCode();
-        checkerAge = 1;
+		arrPersons.sort(sortNumberMax);
+		optomizedCode();
+		checkerAge = 1;
     } else if (event.target.id === `columnID2` && checkerAge === 1) {
-        arrPersons.sort(sortNumberMin);
-        optomizedCode();
-        checkerAge = 0;
-    }
-    if (event.target.id === `columnID3` && checkerCompany === 0) {
-        arrPersons.sort(sortStrCompanyMax);
-        optomizedCode();
-        checkerCompany = 1;
-    } else if (event.target.id === `columnID3` && checkerCompany === 1) {
-        arrPersons.sort(sortStrCompanyMin);
-        optomizedCode();
-        checkerCompany = 0;
-    }
+		arrPersons.sort(sortNumberMin);
+		optomizedCode();
+		checkerAge = 0;
+	}
+	if (event.target.id === `columnID3` && checkerCompany === 0) {
+		arrPersons.sort(sortStrCompanyMax);
+		optomizedCode();
+		checkerCompany = 1;
+	} else if (event.target.id === `columnID3` && checkerCompany === 1) {
+		arrPersons.sort(sortStrCompanyMin);
+		optomizedCode();
+		checkerCompany = 0;
+	}
 }
 listName.addEventListener(`click`, sortUnit);
 
@@ -262,24 +262,25 @@ listName.addEventListener(`click`, sortUnit);
 
 let innerBlock = document.querySelector(`#innerBlock`);
 let squareBlock = document.querySelector(`#squareBlock`);
+let lol = document.querySelector(`#lol`);
+let checkDrop = 0;
 
 function changeWindow(event) {
-    squareBlock.style.zIndex = 1000;
-    document.body.append(squareBlock);
-    moveAt(event.pageX, event.pageY);
-    function moveAt(pageX, pageY) {
-        squareBlock.style.left = pageX - ball.offsetWidth / 2 + 'px';
-        squareBlock.style.top = pageY - ball.offsetHeight / 2 + 'px';
-    }
+	if (event.target.id === squareBlock.id) {
+		checkDrop = 1;
+	}
+}
+function changeWindowOff(event) {
+	if (event.target.id === squareBlock.id) {
+		checkDrop = 0;
+	}
 }
 function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-ball.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    ball.onmouseup = null;
-}
-
-}
-innerBlock.addEventListener(`mousedown`, changeWindow);
+	if (checkDrop === 1) {
+		innerBlock.style.setProperty(`--widthBlock`, `${event.pageX}px`);
+		innerBlock.style.setProperty(`--heightBlock`, `${event.pageY - 830}px`);
+		}
+	}
+squareBlock.addEventListener(`mousedown`, changeWindow);
+squareBlock.addEventListener(`mouseup`, changeWindowOff);
 document.addEventListener('mousemove', onMouseMove);
-
